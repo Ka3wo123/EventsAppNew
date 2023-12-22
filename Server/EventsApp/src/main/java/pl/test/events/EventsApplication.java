e42b91694f6831a394657c8356c9206e3b02bf10
Kajetan Nowak <kajtek2n@gmail.com>
2023-12-22 15:51:14 +0100

    TODO just for test

--- Server/EventsApp/src/main/java/pl/test/events/EventsApplication.java
+++ Server/EventsApp/src/main/java/pl/test/events/EventsApplication.java
@@ -3,6 +3,7 @@ package pl.test.events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// todo zrobic dokumentacje w javadoc
 @SpringBootApplication
 public class EventsApplication {
 
