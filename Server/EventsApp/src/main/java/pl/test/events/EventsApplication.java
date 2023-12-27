b8ee8880d1406179d2306376333a2c7e69e58454
Kajetan Nowak <kajtek2n@gmail.com>
2023-12-27 22:40:26 +0100

    Added build-and-deploy.yml

--- Server/EventsApp/src/main/java/pl/test/events/EventsApplication.java
+++ Server/EventsApp/src/main/java/pl/test/events/EventsApplication.java
@@ -2,11 +2,13 @@ package pl.test.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // todo zrobic dokumentacje w javadoc
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("classpath:application-development.properties")
 public class EventsApplication {
 
 	public static void main(String[] args) {
